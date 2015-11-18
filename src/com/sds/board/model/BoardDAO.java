58cdb8abb4d3e1e686f943d756003ec9caf1cee8
ear26 <ear26@naver.com>
2015-11-18 19:56:40 +0900

    2차커밋

---
+++ src/com/sds/board/model/BoardDAO.java
@@ -0,0 +1,12 @@
+package com.sds.board.model;
+
+import java.util.List;
+
+public interface BoardDAO {
+	public int insert(Board board);
+	public List selectAll();
+	public Board select(int board_id);
+	public int update(Board board);
+	public int delete(int board_id);
+	
+}
